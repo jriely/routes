13724f5485d23bf05130d196949806d858a61ffd
James Riely <jriely@cs.depaul.edu>
2016-11-08 17:53:38 -0600

    Commiting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component} from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector:'app-root',
+    styles:[`
+a{
+    text-decoration: none;
+}
+
+a.active{
+    font-weight: bold;
+}
+`],
+    template: `
+<nav>
+    <a *ngFor="let nav of navs" 
+        [routerLink]="nav.url" 
+        routerLinkActive="active"
+        [routerLinkActiveOptions]="{exact:true}"
+        >
+        {{nav.content}}
+    </a>
+</nav>
+<router-outlet></router-outlet>
+`
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent{
+    navs = [
+        {url: "", content: "Home"},
+        {url: "contacts", content: "Contacts"},
+        {url: "contacts/1", content: "One"},
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+import {NgModule} from "@angular/core";
+import appRoutes from "./app.routes";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:[BrowserModule, appRoutes],
+    declarations:[AppComponent],
+    bootstrap:[AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
